43806afe84b8ff0f4f8c60b7ab414737fa9f7f6c
emaicorp <emaicorp@gmail.com>
2024-04-30 12:47:22 +0100

    80% complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
-let toggle1 =document.querySelector('.toggle1-div')
+window.addEventListener('DOMContentLoaded', () =>{
+
+    let toggle1 =document.querySelector('.toggle1-div')
 let toggle2 =document.querySelector('.toggle2-div')
 toggle1.addEventListener('click', ()=>{
     toggle1.classList.toggle('active');
@@ -10,6 +12,19 @@ toggle2.addEventListener('click', ()=>{
 document.querySelector('.cancel-button').addEventListener('click', ()=>{
     document.querySelector('.side-bar').classList.toggle('active');  
 })
+function handleScreenWidthChange() {
+    var screenWidth = window.innerWidth;
+    const detailHeight = document.querySelector('.details-element.active').offsetHeight;
+    document.querySelector('.mark2').style.marginTop = detailHeight +  100 + 'px';
+
+}
+handleScreenWidthChange()
+
+
+window.addEventListener('resize', function() {
+    handleScreenWidthChange();
+});
+
 
  const aboutBtn = document.querySelectorAll('.about-btn');
  const detailsElement = document.querySelectorAll('.details-element');
@@ -24,9 +39,39 @@ function handleActive(){
         element.classList.remove('active');
         if(data == btnData) {
             element.classList.add('active');
+            handleScreenWidthChange()
         }
     })
 }
  aboutBtn.forEach(btn => {
     btn.addEventListener('click', handleActive);
  })
+
+
+function reveal(element){
+    let reveals = document.querySelectorAll(`${element}`)
+    for(let i = 0; i<reveals.length; i++){
+         let windowHeight = window.innerHeight;
+         let revealTop = reveals[i].getBoundingClientRect().top;
+         let revealPoint = 150;
+         
+
+         if(revealTop < windowHeight - revealPoint  ){
+            reveals[i].classList.add('active');
+       
+                    //  console.log(reveals[i])
+
+
+         }else{
+            reveals[i].classList.remove('active');
+         }
+    }
+}
+
+ window.addEventListener("scroll", ()=>{
+    reveal('.translateY')
+    reveal('.translateYN')
+    reveal('.pop')
+    reveal('.flip')
+ })
+})
